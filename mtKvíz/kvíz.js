5ae88484f0bc1b72ac4eaf85ae7f36f2aff24b1d
SemseyDorka <37298534+SemseyDorka@users.noreply.github.com>
2020-09-29 12:03:28 +0200

    Kvíz alap + pár kérdés

---
+++ mtKvíz/kvíz.js
@@ -0,0 +1,88 @@
+// pos is position of where the user in the test or which question they're up to
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
+var questions = [
+  {
+      question: "Mikor született Mária Terézia?",
+      a: "1723",
+      b: "1720",
+      c: "1717",
+      answer: "C"
+    },
+  {
+      question: "Hány évig uralkodott",
+      a: "40",
+      b: "49",
+      c: "28",
+      answer: "A"
+    },
+  {
+      question: "Melyik dokumentummal biztosította III.Károly a Habsburg-ház nőági örökösödését?",
+      a: "Magna Charta Libertatum",
+      b: "Ratio Educationis",
+      c: "Pragmatica Sanctio",
+      answer: "C"
+    },
+  {
+      question: "Ki volt Mária Terázia férje?",
+      a: "V. Vilmos",
+      b: "I.Ferenc",
+      c: "VIII. Hemrik",
+      answer: "B"
+    }
+  ];
+
+function get(x){
+  return document.getElementById(x);
+}
+
+
+function renderQuestion(){
+  test = get("test");
+  if(pos >= questions.length){
+    test.innerHTML = "<h2>"+correct+" jó válasz a  "+questions.length+"-ből</h2>";
+    get("test_status").innerHTML = "Kvíz vége";
+    // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
+  }
+
+  get("test_status").innerHTML = "A(z) "+(pos+1)+". kérdés a "+questions.length+"-ből";
+  
+  question = questions[pos].question;
+  chA = questions[pos].a;
+  chB = questions[pos].b;
+  chC = questions[pos].c;
+
+  
+  test.innerHTML = "<h3>"+question+"</h3>";
+
+
+  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+  test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+
+function checkAnswer(){
+
+  choices = document.getElementsByName("choices");
+  for(var i=0; i<choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
+    }
+  }
+
+  if(choice == questions[pos].answer){
+
+    correct++;
+  }
+
+  pos++;
+
+  renderQuestion();
+}
+
+window.addEventListener("load", renderQuestion);
